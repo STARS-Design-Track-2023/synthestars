468e7c9430eafc8b332e347dc89dcaa94604862d
ECN Guset015 <ecng015@potr236lnx08.ecn.purdue.edu>
2023-06-22 11:22:56 -0400

    Added second flip flop to synchronizer

--- source/synchronizer.sv
+++ source/synchronizer.sv
@@ -2,6 +2,10 @@ module synchronizer (
 	input logic in, clk,
 	output logic out
 );
+	logic delay;
 	always_ff @(posedge clk)
-		out <= in;
+		delay <= in;
+	
+	always_ff @(posedge clk)
+		out <= delay;
 endmodule
