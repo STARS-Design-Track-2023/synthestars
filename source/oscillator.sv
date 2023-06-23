8ab9ccdcc94db86f0f13641ed1f456fe85179118
ECN Guset023 <ecng023@potr236lnx13.ecn.purdue.edu>
2023-06-23 16:49:37 -0400

    Fixed oscillator

--- source/oscillator.sv
+++ source/oscillator.sv
@@ -4,40 +4,36 @@ Author - Diego Lopez
 Date   - June 22, 2023 
 *********************************************************************************/
 module oscillator #(parameter COUNTS = 149)
-        (input logic clk, nRst, enable, input logic [1:0] octve_dwn, 
-        output logic [7:0] count, divisor);
-    logic [9:0] nxt_sample, sample;
-    logic [7:0] nxt_count;
-    
-    always @(octve_dwn) 
-        divisor = enable ? ((COUNTS << 8) << octve_dwn): 0;
+        (input logic clk, nRst, enable, 
+        output logic [7:0] saw_wave, divisor);
+    logic [7:0] nxt_saw;
+    logic [9:0] nxt_count, count;
 
-    // Clock Divider Sampler
+    always_comb begin
+        if(count >= COUNTS)
+            nxt_count = 1;
+        else
+            nxt_count = count + 1;
+    end
     always_ff @(posedge clk, negedge nRst) begin
         if(!nRst)
-            sample = 0;
-        else
-            sample = enable ? nxt_sample : 0;
-    end
-    always_comb
-        if(count >= COUNTS)
-            nxt_sample = 1;
-        else
-            nxt_sample = sample + 1;
-
-    
-    // Sample Output
-    always_ff @ (posedge sample, negedge nRst) begin
-        if(!nRst)
             count = 0;
-        else
-            count = enable ? nxt_count : 0;
+        else   
+            count = nxt_count;
     end
 
     always_comb begin
-        if (count >= 255)
-            nxt_count = 0;
+        if (saw_wave >= 255)
+            nxt_saw = 0;
+        else if (count == COUNTS)
+            nxt_saw = saw_wave + 1;
         else
-            nxt_count = count + 1;
+            nxt_saw = saw_wave;
+    end
+    always_ff @(posedge clk, negedge nRst) begin
+        if(!nRst)
+            saw_wave = 0;
+        else
+            saw_wave = nxt_saw;
     end
 endmodule
