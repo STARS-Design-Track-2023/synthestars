cd58f94eeafe48ac2ae7853fa3da35c1a909cce3
silenxia <emmoren47@gmail.com>
2023-06-23 13:23:54 -0400

    Mixer part 2 lol

--- source/mixer.sv
+++ source/mixer.sv
@@ -3,14 +3,14 @@ Module - Mixer
 Author - Emily Moreno
 Date   - June 22, 2023 
 ////////////////////////////////////////////////////////////////////////////////*/
-module mixer(input logic [12:0]note, output logic mixed_sample);
+module mixer(input logic [7:0]n1, n2, n3, n4, n5, n6, n7, n8, n9, n10, n11, n12, n13, output logic [11:0]mixed_sample);
 
-logic max;
-assign max = 8'b11111111;
+logic [11:0]max;
+assign max = '1;
 
 always_comb begin
 
-mixed_sample = note[0] + note[1] + note[2] + note[3] + note[4] + note[5] + note[6] + note[7] + note[8] + note[9] + note[10] + note[11] + note[12]; 
+mixed_sample = n1 + n2 + n3 + n4 + n5 + n6 + n7 + n8 + n9 + n10 + n11 + n12 + n13;
 mixed_sample = mixed_sample > max ? max : mixed_sample;
 end 
 // mr code pls work
