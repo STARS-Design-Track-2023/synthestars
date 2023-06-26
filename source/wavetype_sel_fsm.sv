cfeee86b0dc0af0b10db61bf02ec2de9a0011d55
stephclose <closes@purdue.edu>
2023-06-26 16:19:31 -0400

    added wavetype fsm

--- source/wavetype_sel_fsm.sv
+++ source/wavetype_sel_fsm.sv
@@ -1,9 +1,16 @@
 `default_nettype none
-module wavetype_sel_fsm (
-    // HW
-    input logic wave_type, output logic [1:0]type_switch);
+module wavetype_sel_fsm (input logic clk, nrst, wave_type, output logic [1:0]type_switch);
 
 logic [1:0]next_q;
+
+always_ff @ (posedge clk, negedge nrst) begin
+if(nrst == 0) begin
+    type_switch <= 2'b00;
+end
+else begin
+    type_switch <= next_q;
+end
+end
 
 always_comb begin
    case(type_switch)
